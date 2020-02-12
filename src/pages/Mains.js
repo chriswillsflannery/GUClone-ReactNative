92da9d4404d3d1bcafc56aae01e073b36c3e33aa
Chris Flannery <chriswillsflannery@gmail.com>
2020-02-12 13:23:11 -0500

    rendered mains

--- src/pages/Mains.js
+++ src/pages/Mains.js
@@ -1,9 +1,38 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { Text } from 'react-native';
 
 class Mains extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mains: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://s3.amazonaws.com/staginggooduncledigests/products_istcki0x000h28d97a9rv9jp.json')
+      .then(res => res.json())
+      .then(suc => {
+        this.setState(prevState => {
+          const mains = [...prevState.mains];
+          suc.digestData.mains.forEach(main => {
+            mains.push(<Text>{main.name}</Text>);
+          });
+          return {
+            mains: mains,
+          };
+        });
+      });
+  }
+
   render() {
-    return <Text>Mains</Text>;
+
+    return (
+      <>
+        {this.state.mains}
+      </>
+    );
   }
 }
 
