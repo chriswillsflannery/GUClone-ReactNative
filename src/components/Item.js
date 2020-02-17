599b066420223dcc4c9fd3fe2d20775ef732021c
Chris Flannery <chriswillsflannery@gmail.com>
2020-02-17 13:09:41 -0500

    add images to mains

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+} from 'react-native';
+
+const handleAdd = () => {
+  console.log('added to cart');
+};
 
 const Item = ({ title }) => {
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.item}>
+        <Image
+          style={styles.image}
+          source={require('../../images/pasta.jpg')}
+        />
+        <Text>{title}</Text>
+        <Button title="+ 1" onPress={() => handleAdd()} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 40,
+    height: 40,
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 10,
+    margin: 5,
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'black',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
 
 export default Item;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -16,12 +16,10 @@ const Cart = () => {
   return (
     <>
       <SafeAreaView>
-        <View style={styles.separator} />
         <View style={styles.fixToText}>
           <Button title="Log Out" onPress={() => handleLogout()} />
           <Button title="Back to Mains" onPress={() => handleMains()} />
         </View>
-        <View style={styles.separator} />
       </SafeAreaView>
     </>
   );
@@ -31,9 +29,6 @@ const styles = StyleSheet.create({
   fixToText: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
   },
 });
 
